4d2a953e7bc56fdbd24503d0db67eaa6ed69a2f2
jhd235 <jhd235@outlook.com>
2020-05-17 21:38:19 +0500

    articles

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
-
 // connect to database
-// coming soon...
+//$conn = mysqli_connect("srv-pleskdb20.ps.kz:3306", "simple", "3Xs94^NZ1SW&", "indxkz_simple");
+$conn = mysqli_connect("localhost", "simple", "3Xs94^NZ1SW&", "simple");
 
+if (!$conn) {
+    die("Error connecting to database: " . mysqli_connect_error());
+}
+// define global constants
 define ('ROOT_PATH', realpath(dirname(__FILE__)));
-define('BASE_URL', 'http://simplePHP/');
+define('BASE_URL', 'http://simplephp/');
 ?>
